f1022a4eedb6469c50b418b1b2af6fd75f88accc
Surya Stefanus <suryastef@gmail.com>
2021-08-01 00:11:13 +0700

    change brand to icon

--- resources/views/layouts/frontend/app.blade.php
+++ resources/views/layouts/frontend/app.blade.php
@@ -36,7 +36,7 @@
   <!-- ======= Header/ Navbar ======= -->
   <nav class="navbar navbar-b navbar-trans navbar-expand-md fixed-top" id="mainNav">
     <div class="container">
-      <a class="navbar-brand js-scroll" href="#page-top">{{ $getHeader->navbar_title ?? 'MY-PFOLIO' }}</a>
+      <a class="navbar-brand js-scroll" href="#page-top"> <img alt="Github" src="/images/git.png" width="30" height="30"></a>
       <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarDefault" aria-controls="navbarDefault" aria-expanded="false" aria-label="Toggle navigation">
         <span></span>
         <span></span>
